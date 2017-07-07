9ff8919945ab50a18798478c716fb8f9b3325ea2
agulzadian <a.gulzadian@gmail.com>
2017-07-07 15:58:12 +0200

    initial commit before fishing trip

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var base = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec."
 
+var split = base.split(" ");
+
+
+
 var button = document.querySelector("#generate-button");
 var outputArea = document.querySelector("#output-area");
 
@@ -8,11 +12,10 @@ button.addEventListener("click", generate);
 // add input fields -  start off with just words count
 
 function generate() {
-  outputArea.innerHTML = "<p>" + base + "</p>";
+
+  outputArea.innerHTML = "<p>" + split.slice(0,2) + "</p>";
   // note that now the base text is inserted in the div, this needs to be changed to the trimmed (or multiplied)
   // version of the base
 }
 
 // add a clear button
-
-console.log(base[2]);
