d16be0b06e92da27cceeeb7586e01d7b1ca513cd
agulzadian <a.gulzadian@gmail.com>
2017-07-14 17:05:45 +0200

    basic generation function works, including a fix for max word count.

--- main.js
+++ main.js
@@ -1,33 +1,42 @@
-// var base = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec. "
+var base = "Lorem ;ipsum dolor; sit amet, ; consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor.  Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec.";
 
-var base = "Lorem ipsum ";
-var base1 = "Lorem ipsum ";
+// var base = "Lorem ipsum dolor sit Amet";
 
-var split = base.split(" ");
+base = base.replace(/\,/g,"");
+base = base.replace( /\./g ,"");
+base = base.replace(/\;/g,"");
+base = base.replace(/  /g," ");
+base = base.replace(/\"/g, "");
 
-var button = document.querySelector("#generate-button");
-var outputArea = document.querySelector("#output-area");
+var lorem = base; //base is trimmed
 
-button.addEventListener("click", generate);
+var loremArray = lorem.split(" "); //text is turned into an array in order to be able to select x amount of words
 
-// add input fields -  start off with just words count
+var numbWord = 300;
 
-var numbWord = document.querySelector("#number-of-words");
-
-button.formAction = "#";
-
-function generate(end) {
-
-  end = numbWord.value;
-
-  if (end > split.length) {
-    base += base;
-  }
-
-  outputArea.innerHTML = "<p>" + split.slice(0, end) + "</p>";
-
+if (numbWord > loremArray.length){
+  loremArray = loremArray.concat(loremArray.slice(0,numbWord - loremArray.length));
 }
 
 
+loremArray = loremArray.slice(0,numbWord);// amount of words is selected
 
-// add a clear button
+var displayLorem = loremArray.join(" ");//text is made back into a string for displaying
+
+console.log(displayLorem);
+
+
+
+
+
+
+
+
+
+
+
+// var test = ["lol", "wtf", "omg"];
+//
+// test = test.concat(test.slice(0,2));
+//
+// console.log(test);
