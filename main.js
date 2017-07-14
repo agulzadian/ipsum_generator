7f4a46292faedae262f99d925951b5890454e949
agulzadian <a.gulzadian@gmail.com>
2017-07-14 23:00:26 +0200

    generator works with user input, now need to fix the paragraph length functionality, notes on this in comments

--- main.js
+++ main.js
@@ -1,36 +1,30 @@
 var base = "Lorem ;ipsum dolor; sit amet, ; consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor.  Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec.";
 // var base = "Lorem ipsum dolor sit Amet";
 
-base = base.replace(/\,/g,"");
-base = base.replace( /\./g ,"");
-base = base.replace(/\;/g,"");
+base = base.replace(/\,/g,"");// the /x/g means the effect is applied to all
+base = base.replace( /\./g ,"");// instances, not just the first encounter,
+base = base.replace(/\;/g,"");// the \ makes sure the effect of the dot, comma etc. are negated
 base = base.replace(/  /g," ");
 base = base.replace(/\"/g, "");
 
 var lorem = base; //base is trimmed
 var loremArray = lorem.split(" "); //text is turned into an array in order to be able to select x amount of words
-var numbWordInput = document.querySelector("#number-of-words").value;
-
-
+var loremArrayUsed;//need a seperate array because the way I used slice() in the generate function alters the original lorenArray
+var button = document.querySelector("#generate-button");
+button.formAction = "#";// to cancel out the annoying reloading of the page on enter
+var numbWordInput = document.querySelector("#number-of-words");
+numbWordInput.autofocus = true;
+var outputArea = document.querySelector("#output-area");
+var numbWord;
+var displayLorem;//the actual string that gets shown
 
 //=============================== USER INPUT ======================//
 
-//----select number of words
-var numbWord = 10;
-
-loremArray = loremArray.slice(0,numbWord);// amount of words is selected
-
-
-
-if (numbWord > loremArray.length){ //adds words to the array if there aren't enought words in lorem
-  loremArray = loremArray.concat(loremArray.slice(0,numbWord - loremArray.length));
-}
-
 //----select paragraph length
 
-function setParLength(length) {
-  length = 6; // set to user input field
-  loremArray.splice(length, 0, "\n");
+function setParLength(length) {// has a bug: only breaks after the first length of words, need to make it breaks every length of words (so every 50 words), can do this using modulo
+  length = 50; // set to user input field
+  loremArray.splice(length, 0, "\n\n");//add a break
 }
 
 setParLength();
@@ -38,24 +32,17 @@ setParLength();
 
 //==================================================================//
 
-var displayLorem = loremArray.join(" ");//text is made back into a string for displaying
 
-console.log(displayLorem);
+function generate(){
+  numbWord = numbWordInput.value;//numb of words is selected
+  loremArrayUsed = loremArray.slice(0,numbWord);// amount of words is actually processed
+  displayLorem = loremArrayUsed.join(" ");//text is made back into a string for displaying
+  console.log(displayLorem);
+}
+
+button.addEventListener("click", generate);
+
 
 // be able to select number of words OR number of characters --> can be done in two ways:
 // 1: be able to select char OR words for the same input field
 // 2: make two input fields and have one of them inactive/greyed oud if the other one is focused
-
-
-
-
-
-
-
-
-
-// var test = ["lol", "wtf", "omg"];
-//
-// test = test.concat(test.slice(0,2));
-//
-// console.log(test);
