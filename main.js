29e62f73252216da0a8072a8b459dbcef9525db9
agulzadian <a.gulzadian@gmail.com>
2017-07-15 23:39:41 +0200

    got capitalisation to work, now working on the periods and commas but can't continue till I fix bugg that limits the output copy to 250-ish words

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ base = base.replace( /\./g ,"");// instances, not just the first encounter,
 base = base.replace(/\;/g,"");// the \ makes sure the effect of the dot, comma etc. are negated
 base = base.replace(/  /g," ");
 base = base.replace(/\"/g, "");
+base = base.toLowerCase();
 
 var lorem = base; //base is trimmed
 var loremArray = lorem.split(" "); //text is turned into an array in order to be able to select x amount of words
@@ -14,9 +15,14 @@ var button = document.querySelector("#generate-button");
 button.formAction = "#";// to cancel out the annoying reloading of the page on enter
 var numbWordInput = document.querySelector("#number-of-words");
 numbWordInput.autofocus = true;
+var parLengthInput = document.querySelector("#words-per-paragraph")
+parLengthInput.defaultValue = 100;
 var outputArea = document.querySelector("#output-area");
 var numbWord;
 var displayLorem;//the actual string that gets shown
+var parLength;
+var ranPeriod = Math.floor((Math.random()*10+5));
+var periodCounter = 0;
 
 //=============================== USER INPUT ======================//
 
@@ -26,19 +32,38 @@ function generate(){
   loremArrayUsed = loremArray.slice(0,numbWord);// amount of words is actually processed
 
   //----select paragraph length
+  parLength = parLengthInput.value;
 
   for (i = 0; i < numbWord; i++){
 
-    if(i % 100 == 0 && i != 0){
-      loremArrayUsed.splice(i, 0, "\n\n");//add a break
+    if(i % parLength == 0 && i != 0 && i < loremArrayUsed.length){
+      loremArrayUsed.splice(i, 0, "\.\n\n");//add a break
+      loremArrayUsed[i+1] = loremArrayUsed[i+1].charAt(0).toUpperCase() + loremArrayUsed[i+1].slice(1, loremArrayUsed.length);
+      periodCounter ++;
+      console.log(periodCounter);
     }
+
+    // if(i % ranPeriod == 0 && i != 0 && i < loremArrayUsed.length) {
+    //   loremArrayUsed.splice(i, 0, "\.\"\,");
+    //   loremArrayUsed[i+1] = loremArrayUsed[i+1].charAt(0).toUpperCase() + loremArrayUsed[i+1].slice(1, loremArrayUsed.length);
+    //   ranPeriod = Math.floor((Math.random()*10+5));
+    //   console.log(ranPeriod);
+    //   }
+
+
   }
 
+
+
+
+  loremArrayUsed.push(".");//adds the final period after the generated copy, not sure why specifically that period was missing tho
   displayLorem = loremArrayUsed.join(" ");//text is made back into a string for displaying
   displayLorem = displayLorem.replace(/\n\n /g,"\n\n");//delete space before punctuation
   displayLorem = displayLorem.replace(/ \,/g,",");//delete space before punctuation
   displayLorem = displayLorem.replace(/ \./g,".");
   console.log(displayLorem);
+
+
 }
 
 button.addEventListener("click", generate);
@@ -46,13 +71,11 @@ button.addEventListener("click", generate);
 
 
 
-// function setParLength(length) {// has a bug: only breaks after the first length of words, need to make it breaks every length of words (so every 50 words), can do this using modulo
-//   length = 50; // set to user input field
-//   loremArray.splice(length, 0, "\n\n");//add a break
-// }
 
 // be able to select number of words OR number of characters --> can be done in two ways:
 // 1: be able to select char OR words for the same input field
 // 2: make two input fields and have one of them inactive/greyed oud if the other one is focused
 
 // need to fix bug with the form action thing -> first time you hit enter it clears everything again and only then works
+
+// need to fix upper case
