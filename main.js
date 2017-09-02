4bcc46d0b5ea6de0bf3d557931230d586ad90ab8
agulzadian <a.gulzadian@gmail.com>
2017-09-02 15:29:39 +0200

    refactoring and bug fixing, max wordcount fixed

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ base = base.replace(/[^\w\s]/g,"") //regex here selects everything that is not a
 base = base.replace(/  /g," ");
 
 var loremArray = base.split(" "); //text is turned into an array in order to be able to select x amount of words and punctuation at random intervals
-var loremArrayUsed;//need a seperate array because the way I used slice() in the generate function alters the original lorenArray
+var loremArray;//need a seperate array because the way I used slice() in the generate function alters the original lorenArray
 var button = document.querySelector("#generate-button");
 button.formAction = "#";// to cancel out the annoying reloading of the page on enter
 var numbWordInput = document.querySelector("#number-of-words");
@@ -27,16 +27,17 @@ var periodCounter = 0;
 var ranComma = 6;
 var commaCounter = 0;
 
-//=============================== USER INPUT ======================//
+//=============================== GENERATION ======================//
 
 function generate(){
   numbWord = numbWordInput.value;//numb of words is selected
 
-  if (numbWord > loremArray.length){    //max word count limit fix
+  while (numbWord > loremArray.length){    //max word count limit fix
     loremArray = loremArray.concat(loremArray.slice(0,numbWord - loremArray.length));
   }
 
-  loremArrayUsed = loremArray.slice(0,numbWord);// amount of words is actually processed
+
+  loremArray = loremArray.slice(0,numbWord);// amount of words is actually processed
 
   parLength = parLengthInput.value;   //----select paragraph length
 
@@ -45,32 +46,33 @@ function generate(){
     periodCounter ++;
     commaCounter ++;
 
-    if(i % parLength == 0 && i != 0 && i < loremArrayUsed.length){
-      loremArrayUsed.splice(i, 0, "\.\n\n");//add a break
-      loremArrayUsed[i+1] = loremArrayUsed[i+1].charAt(0).toUpperCase() + loremArrayUsed[i+1].slice(1, loremArrayUsed[i+1].length);
+    if(i % parLength == 0 && i != 0 && i < loremArray.length){
+      loremArray.splice(i, 0, "\.\n\n");//add a break
+      loremArray[i+1] = loremArray[i+1].charAt(0).toUpperCase() + loremArray[i+1].slice(1, loremArray[i+1].length);
     }
 
     if (periodCounter == ranPeriod){
-      loremArrayUsed.splice(i, 0, "\.");//add a period and one line below: add capital letter after period
-      loremArrayUsed[i+1] = loremArrayUsed[i+1].charAt(0).toUpperCase() + loremArrayUsed[i+1].slice(1, loremArrayUsed[i+1].length);
+      loremArray.splice(i, 0, "\.");//add a period and one line below: add capital letter after period
+      loremArray[i+1] = loremArray[i+1].charAt(0).toUpperCase() + loremArray[i+1].slice(1, loremArray[i+1].length);
       periodCounter = 0;
       ranPeriod =  Math.floor(Math.random() * (15 - 5 + 1)) + 5;
     }
 
     if (commaCounter == ranComma){
-      loremArrayUsed.splice(i, 0, "\,");//add a comma and one line below: add capital letter after period
+      loremArray.splice(i, 0, "\,");//add a comma and one line below: add capital letter after period
       commaCounter = 0;
       ranComma =  Math.floor(Math.random() * (15 - 5 + 1)) + 5;
     }
   }
 
-  loremArrayUsed.push(".");//adds the final period after the generated copy, not sure why specifically that period was missing tho
-  displayLorem = loremArrayUsed.join(" ");//text is made back into a string for displaying
+  loremArray.push(".");//adds the final period after the generated copy, not sure why specifically that period was missing tho
+  displayLorem = loremArray.join(" ");//text is made back into a string for displaying
   displayLorem = displayLorem.replace(/\n\n /g,"\n\n");//delete space before punctuation
   displayLorem = displayLorem.replace(/ \,/g,",");//delete space before comma
   displayLorem = displayLorem.replace(/ \./g,".");//delete space before period
   displayLorem = displayLorem.replace(/\,\./g,".");//delete comma if there's a period after a comma (sometimes happens because of the randomness of comma and period insertion)
   displayLorem = displayLorem.replace(/\.\,/g,".");//same as above but in reverse order
+  displayLorem = displayLorem.replace(/\. \,/g,".");
 
   // console.log(JSON.stringify(displayLorem));// testing out the stringify command
   outputArea.innerHTML = displayLorem;
